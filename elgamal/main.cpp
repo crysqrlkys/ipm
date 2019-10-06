d88e1567f936a4c948bf7f0f2add040afc6b22fb
crysqrl <whykonstantin@gmail.com>
2019-10-06 17:30:55 +0300

    Refactor code

--- elgamal/main.cpp
+++ elgamal/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <tuple>
 #include <unordered_set>
+#include <sstream>
+#include <codecvt>
 
 
 using namespace std;
@@ -24,23 +26,65 @@ public:
 
 class PrivateKey {
 public:
-    PrivateKey(long long _p, long long _g, long long _x, long long _iNumBits) {
-        p = _p;
-        g = _g;
+    PrivateKey(long long _x) {
         x = _x;
-        iNumBits = _iNumBits;
     }
 
-    long long p;
-    long long g;
     long long x;
-    long long iNumBits;
 };
 
-long long gcd(long long a, long long b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+std::wstring utf8_to_utf16(const std::string &utf8) {
+    std::vector<unsigned long> unicode;
+    size_t i = 0;
+    while (i < utf8.size()) {
+        unsigned long uni;
+        size_t todo;
+        bool error = false;
+        unsigned char ch = utf8[i++];
+        if (ch <= 0x7F) {
+            uni = ch;
+            todo = 0;
+        } else if (ch <= 0xBF) {
+            throw std::logic_error("not a UTF-8 string");
+        } else if (ch <= 0xDF) {
+            uni = ch & 0x1F;
+            todo = 1;
+        } else if (ch <= 0xEF) {
+            uni = ch & 0x0F;
+            todo = 2;
+        } else if (ch <= 0xF7) {
+            uni = ch & 0x07;
+            todo = 3;
+        } else {
+            throw std::logic_error("not a UTF-8 string");
+        }
+        for (size_t j = 0; j < todo; ++j) {
+            if (i == utf8.size())
+                throw std::logic_error("not a UTF-8 string");
+            unsigned char ch = utf8[i++];
+            if (ch < 0x80 || ch > 0xBF)
+                throw std::logic_error("not a UTF-8 string");
+            uni <<= 6;
+            uni += ch & 0x3F;
+        }
+        if (uni >= 0xD800 && uni <= 0xDFFF)
+            throw std::logic_error("not a UTF-8 string");
+        if (uni > 0x10FFFF)
+            throw std::logic_error("not a UTF-8 string");
+        unicode.push_back(uni);
+    }
+    std::wstring utf16;
+    for (size_t i = 0; i < unicode.size(); ++i) {
+        unsigned long uni = unicode[i];
+        if (uni <= 0xFFFF) {
+            utf16 += (wchar_t) uni;
+        } else {
+            uni -= 0x10000;
+            utf16 += (wchar_t) ((uni >> 10) + 0xD800);
+            utf16 += (wchar_t) ((uni & 0x3FF) + 0xDC00);
+        }
+    }
+    return utf16;
 }
 
 bool isPrime(long long number) {
@@ -65,6 +109,17 @@ long long modulo(long long base, long long exponent, long long mod) {
         exponent = exponent / 2;
     }
     return x % mod;
+}
+
+long long mul(long long a, long long b, long long n) {// a*b mod n
+    int sum = 0;
+    for (int i = 0; i < b; i++) {
+        sum += a;
+        if (sum >= n) {
+            sum -= n;
+        }
+    }
+    return sum;
 }
 
 void findPrimefactors(unordered_set<int> &s, long long n) {
@@ -106,42 +161,6 @@ int findPrimitiveRoot(long long n) {
     return -1;
 }
 
-vector<long long> encode(string plainText, long long iNumBits) {
-    vector<char> bytes(plainText.begin(), plainText.end());
-    vector<long long> z;
-    int k = iNumBits / 8;
-    int j = -1 * k;
-
-    for (vector<long long>::size_type i = 0; i != bytes.size(); i++) {
-        if (i % k == 0) {
-            j += k;
-            z.push_back(0);
-        }
-        z[j / k] += bytes[i] * (pow(2, 8 * (i % k)));
-    }
-    return z;
-}
-
-string encrypt(PublicKey pk, string plaintText) {
-    vector<long long> z = encode(plaintText, pk.iNumBits);
-    vector<vector<long long>> cipher_pairs;
-    for (auto const &i : z) {
-        long long range = pk.p + 1;
-        long long k = rand() % range;
-        long long a = modulo(pk.g, k, pk.p);
-        long long b = (i * modulo(pk.y, k, pk.p)) % pk.p;
-        vector<long long> pair;
-        pair.push_back(a);
-        pair.push_back(b);
-        cipher_pairs.push_back(pair);
-    }
-    string encText;
-    for (auto const &pair : cipher_pairs) {
-        encText += to_string(pair[0]) + " " + to_string(pair[1]) + " ";
-    }
-    return encText;
-}
-
 long long findPrime(long long min, long long max) {
     long long range = max - min + 1;
     while (true) {
@@ -151,21 +170,47 @@ long long findPrime(long long min, long long max) {
 }
 
 tuple<PublicKey, PrivateKey> generateKeys() {
-    int iNumBits = 64;
+    int iNumBits = 128;
     long long p = findPrime(100000, 1000000);
     long long g = findPrimitiveRoot(p);
-    g = modulo(g, 2, p);
+
     long long x = rand() % (int) ((p - 1) / 2 - 1 + 1) + 1;
     long long y = modulo(g, x, p);
+
     PublicKey publicKey(p, g, y, iNumBits);
-    PrivateKey privateKey(p, g, x, iNumBits);
+    PrivateKey privateKey(x);
     return make_tuple(publicKey, privateKey);
 }
 
+string crypt(PublicKey pk, vector<char> text) {
+    long long p = pk.p;
+    long long g = pk.g;
+    long long y = pk.y;
+
+    string encText;
+    for (auto const &m : text) {
+        if (m > 0) {
+            long long k = rand() % (p - 2) + 1; // 1 < k < (p-1)
+            long long a = modulo(g, k, p);
+            long long b = mul(modulo(y, k, p), m, p);
+            encText += to_string(a) + " " + to_string(b) + " ";
+        }
+    }
+    return encText;
+}
+
+
 int main() {
     tuple<PublicKey, PrivateKey> a = generateKeys();
-    string t = "My name is Jeff";
-    string cipher = encrypt(get<0>(a), t);
+    string t = "My name is Jeff and Jeff";
+    vector<char> bytes(t.begin(), t.end());
+    string cipher = crypt(get<0>(a), bytes);
+    cout<< "Encoded text: " << endl;
     cout << cipher;
+
+
+
+//    string plain_t = decrypt(get<1>(a), cipher);
+//    cout << plain_t;
     return 0;
 }
