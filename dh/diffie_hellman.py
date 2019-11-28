5f3e3bf63cc63bdbf256dcae08946471c67e0bff
crysqrl <whykonstantin@gmail.com>
2019-11-28 13:52:56 +0300

    DH initial

---
+++ dh/diffie_hellman.py
@@ -0,0 +1,16 @@
+class Curve:
+    pass
+
+
+class DiffieHellman:
+    def __init__(self):
+        pass
+
+    def run(self):
+        return True
+
+
+if __name__ == '__main__':
+    dh = DiffieHellman()
+    s = dh.run()
+
